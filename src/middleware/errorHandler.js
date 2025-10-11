b3cf7e57f87890ee98767e664ed6e8c4fb211f8e
yabluninn <yablunin.artem@gmail.com>
2025-10-11 09:49:10 +0200

    removed unused comments

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -3,11 +3,9 @@ import { HttpError } from 'http-errors';
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
 
-  // если ошибка — экземпляр HttpError
   if (err instanceof HttpError) {
     return res.status(err.status).json({ message: err.message });
   }
 
-  // иначе — общая серверная ошибка
   res.status(500).json({ message: 'Server error' });
 };
--- src/server.js
+++ src/server.js
@@ -10,21 +10,17 @@ import { errorHandler } from './middleware/errorHandler.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3030; // ✅ изменено с 3000 на 3030
+const PORT = process.env.PORT || 3030;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(logger);
 
-// Routes
-app.use(notesRoutes); // ✅ убрали '/notes'
+app.use(notesRoutes);
 
-// Handlers
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-// Start
 const startServer = async () => {
   await connectMongoDB();
   app.listen(PORT, () => {
